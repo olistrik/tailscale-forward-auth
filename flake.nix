2f2bfc1e5a79284705d89fc27ae71edac21dcdbf
Oli Strik <strik@olii.nl>
2025-05-27 11:23:09 +0200

    fix overlay

--- flake.nix
+++ flake.nix
@@ -6,20 +6,23 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { flake-utils, nixpkgs, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+  outputs = { self, flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
 
-        inherit (pkgs) callPackage;
-      in
-      rec {
-        packages.default = callPackage ./default.nix { };
-        devShells.default = callPackage ./shell.nix { };
-        # nixosModules.default = import ./nix/module.nix;
+          inherit (pkgs) callPackage;
+        in
+        {
+          packages.default = callPackage ./default.nix { };
+          devShells.default = callPackage ./shell.nix { };
+          # nixosModules.default = import ./nix/module.nix;
 
-        overlays.default = final: prev: {
-          tailscale-nginx-auth = packages.default;
-        };
-      });
+        }) // {
+
+      overlays.default = final: prev: {
+        tailscale-nginx-auth = self.packages.${final.system}.default;
+      };
+    };
 }
